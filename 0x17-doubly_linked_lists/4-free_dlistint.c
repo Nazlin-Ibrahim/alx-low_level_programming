85fa2e4af05bd58a8341c28986e6e7e50180c5e4
Nazlin-Ibrahim <nazlinibrahim3@gmail.com>
2024-01-19 22:35:58 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,36 +1,18 @@
 #include "lists.h"
+
 /**
-* add_dnodeint_end - function
-* @head: double pointer to first node of list
-* @n: new node's data
-*
-* Description: function to add new node at end of dbl lnkd list
-* Return: address of new element, or NULL if fail
-*/
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+ *free_dlistint - freeing memory allocation of a linked list
+ *@head: a pointer
+ */
+
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *newNode;
-	dlistint_t *lastNode;
-	int num = n;
+	dlistint_t *temp;
 
-	newNode = malloc(sizeof(dlistint_t));
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->n = num;
-	newNode->next = NULL;
-
-	if (*head == NULL)
+	while (head != NULL)
 	{
-		newNode->prev = NULL;
-		*head = newNode;
-		return (newNode);
+		temp = head;
+		head = head->next;
+		free(temp);
 	}
-	lastNode = *head;
-	while (lastNode->next != NULL)
-		lastNode = lastNode->next;
-	lastNode->next = newNode;
-	newNode->prev = lastNode;
-
-	return (newNode);
 }
